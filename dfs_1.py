33118344af9e1822cd429344199992d1417c2d6c
jisuuuu <gjkl55@naver.com>
2020-04-27 20:44:22 +0900

    dfs_1

---
+++ dfs_1.py
@@ -0,0 +1,23 @@
+answer = 0
+def solution(numbers, target):
+    dfs(numbers, target, 0)
+    return answer
+
+def dfs(numbers, target, idx):
+    global answer
+    if idx == len(numbers):
+        sum = 0
+
+        for n in numbers:
+            sum += n
+
+        if sum == target:
+            answer += 1
+        return
+
+    else:
+        numbers[idx] *= 1
+        dfs(numbers, target, idx + 1)
+
+        numbers[idx] *= -1
+        dfs(numbers, target, idx + 1)
