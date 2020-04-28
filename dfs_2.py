7a28dff6520a15ad32eed9396592bf66d6ecca1f
jisuuuu <gjkl55@naver.com>
2020-04-28 22:27:59 +0900

    dfs_2

---
+++ dfs_2.py
@@ -0,0 +1,23 @@
+def solution(n, computers):
+    answer = 0
+    visited = []
+
+    for i in range(0, len(computers)):
+        visited.append(False)
+
+    for idx, v in enumerate(visited):
+        if v == False:
+            answer += 1
+            dfs(idx, visited, computers)
+
+    return answer
+
+def dfs(idx, visited, computers):
+    visited[idx] = True
+
+    for i in range(0, len(computers)):
+        if visited[i] == False and computers[idx][i] == 1:
+            dfs(i, visited, computers)
+
+if __name__ == '__main__':
+    print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
