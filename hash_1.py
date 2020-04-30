2a4bd0985a0e51161b9f77b846fc2e9dfadfe156
jisuuuu <gjkl55@naver.com>
2020-05-01 00:01:49 +0900

    hash_1 완주하지 못한 선수

--- hash_1.py
+++ hash_1.py
@@ -1,3 +1,4 @@
+#완주하지 못한 선수
 def solution(participant, completion):
     participant.sort()
     completion.sort()
@@ -11,5 +12,4 @@ def solution(participant, completion):
 
     if answer == "":
         answer = participant[-1]
-
     return answer
