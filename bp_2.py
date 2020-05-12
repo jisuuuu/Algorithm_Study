c9b6e064eb6d88ffe9d3379329e4134d0f6b23a7
jisuuuu <gjkl55@naver.com>
2020-05-12 15:50:00 +0900

    bp_2 소수찾기

---
+++ bp_2.py
@@ -0,0 +1,29 @@
+#소수찾기
+import itertools
+
+
+def solution(numbers):
+    answer = 0
+    new = []
+
+    for i in range(1, len(numbers) + 1):
+        perm = itertools.permutations(list(numbers), i)
+        for i in list(perm):
+            num = int("".join(list(i)))
+            new.append(num)
+
+    new = set(new)
+
+    for n in new:
+        if n < 2:
+            pass
+        elif n == 2:
+            answer += 1
+        else:
+            for i in range(2, n):
+                if n % i == 0:
+                    break
+                elif i == n - 1:
+                    answer += 1
+
+    return answer
