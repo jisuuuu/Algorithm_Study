8d42275a33ba89000b6f4a0dc266528c3107c24d
Jisu Choi <gjkl55@naver.com>
2021-01-15 20:19:19 +0900

    BOJ5073 삼각형과 세 변

---
+++ Baekjoon/Baekjoon_python/boj_5073.py
@@ -0,0 +1,16 @@
+#삼각형과 세 변
+import sys
+while True:
+    arr = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
+
+    if arr[0] == arr[1] == arr[2] == 0:
+        break
+
+    if arr[0] + arr[1] <= arr[2]:
+        print('Invalid')
+    elif arr[0] == arr[1] == arr[2]:
+        print('Equilateral')
+    elif arr[0] == arr[1] or arr[1] == arr[2] or arr[2] == arr[0]:
+        print('Isosceles')
+    else:
+        print('Scalene')
