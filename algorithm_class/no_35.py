f51f8eecbd092a9d751a113e6f1f2b8ff58f9566
최지수 <gjkl55@naver.com>
2021-05-13 22:13:03 +0900

    35. Special Sort + bubbleSort

--- algorithm_class/no_35.py
+++ algorithm_class/no_35.py
@@ -1,11 +1,21 @@
 #Special Sort
 
+def bubbleSort(temp):
+    for i in range(len(temp) - 1):
+        for j in range(len(temp) - 1 - i):
+            if temp[j] > temp[j + 1]:
+                temp[j], temp[j + 1] = temp[j + 1], temp[j]
+
+    return temp
+
 
 for j in range(1, 6):
     f = open(f'D:/알고리즘 강의자료/CPS(채점폴더)/35. Special Sort/in{j}.txt', 'r')
     n = int(f.readline().rstrip())
     arr = list(map(int, f.readline().rstrip().split()))
 
+    new = bubbleSort(arr)
+    print(new)
     for i in range(len(arr) - 1):
         for k in range(len(arr) - 1 - i):
             if arr[k] > 0 and arr[k + 1] < 0:
