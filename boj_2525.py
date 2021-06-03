7cf7c682e9f9134827655c00ea655d8e5294b450
jisuuuu <gjkl55@naver.com>
2021-06-03 23:28:09 +0900

    BOJ2525 오븐 시계

---
+++ boj_2525.py
@@ -0,0 +1,16 @@
+#오븐 시계
+import sys
+
+a, b = map(int, sys.stdin.readline().rstrip().split())
+c = int(sys.stdin.readline().rstrip())
+
+a += c // 60
+b += c % 60
+
+if b >= 60:
+    a += 1
+    b -= 60
+if a >= 24:
+    a -= 24
+
+print(a, b)
