cc98f078744b21ae1fb8c8a64acde8039dbf6ca3
최지수 <gjkl55@naver.com>
2021-07-16 14:46:29 +0900

    대표값 평균 구하는 방식 오류

--- inflern_algorithm/no_4.py
+++ inflern_algorithm/no_4.py
@@ -1,9 +1,10 @@
 #대표값
 import sys
-#sys.stdin = open("input.txt", "rt")
+sys.stdin = open("input.txt", "rt")
 n = int(input())
 arr = list(map(int, input().split()))
-avg = round(sum(arr) / len(arr))
+# avg = round(sum(arr) / len(arr)) #round_half_even 방식이니 사용하지마
+avg = int(sum(arr) / len(arr) + 0.5) #평균구하는 방식
 min_num = -1
 min_point = float('inf')
 idx = -1
@@ -18,3 +19,7 @@ for i, a in enumerate(arr):
             min_num = a
             idx = i
 print(f'{avg} {idx + 1}')
+
+#round_half_even 정확한 하프지점이 없으면 짝수쪽으로 가버림
+print(round(4.500))
+print(round(4.511))
