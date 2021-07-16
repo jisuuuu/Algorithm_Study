230fe26738a13c7f2961930c72aafa2a0c7a6294
최지수 <gjkl55@naver.com>
2021-07-16 14:41:23 +0900

    대표값

--- inflern_algorithm/no_4.py
+++ inflern_algorithm/no_4.py
@@ -1,6 +1,20 @@
 #대표값
 import sys
-sys.stdin = open("input.txt", "rt")
+#sys.stdin = open("input.txt", "rt")
 n = int(input())
 arr = list(map(int, input().split()))
-avg = sum(arr) // len(arr)
+avg = round(sum(arr) / len(arr))
+min_num = -1
+min_point = float('inf')
+idx = -1
+
+for i, a in enumerate(arr):
+    if abs(avg - a) < min_point:
+        min_point = abs(avg - a)
+        min_num = a
+        idx = i
+    elif abs(avg - a) == min_point:
+        if min_num < a:
+            min_num = a
+            idx = i
+print(f'{avg} {idx + 1}')
