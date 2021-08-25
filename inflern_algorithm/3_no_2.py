877022fff743b24d3e997f6fbf27c72c1e61474f
jisuuuu <gjkl55@naver.com>
2021-08-25 21:57:07 +0900

    숫자만 추출 설명 추가

--- inflern_algorithm/3_no_2.py
+++ inflern_algorithm/3_no_2.py
@@ -1,15 +1,18 @@
 #숫자만 추출
 import sys
 from collections import deque
-# sys.stdin = open("input.txt", "rt")
+sys.stdin = open("input.txt", "rt")
 word = input()
 queue = deque()
-
+res = 0
 for w in word:
+    if w.isdecimal():
+        res = res * 10 + int(w)
     if w.isdigit():
         if w == '0' and not queue:
             continue
         queue.appendleft(w)
+print(res)
 n = 0
 
 for i, q in enumerate(queue):
