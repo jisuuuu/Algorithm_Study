59a65f156958a06fd27659247f8cb21c79bbce8e
Jisu Choi <gjkl55@naver.com>
2021-08-26 22:32:30 +0900

    두 리스트 합치기

--- inflern_algorithm/3_no_4.py
+++ inflern_algorithm/3_no_4.py
@@ -1,12 +1,14 @@
 # 두 리스트 합치기
 import sys
 from collections import deque
-# sys.stdin = open("input.txt", "rt")
+sys.stdin = open("input.txt", "rt")
 n = int(input())
-n_arr = deque(map(int, input().split()))
+a = list(map(int, input().split()))
+n_arr = deque(a)
 m = int(input())
-m_arr = deque(map(int, input().split()))
-i, j = 0, 0
+b = list(map(int, input().split()))
+m_arr = deque(b)
+
 while True:
     if not n_arr:
         break
@@ -22,3 +24,20 @@ if n_arr:
     print(' '.join(list(map(str, n_arr))))
 if m_arr:
     print(' '.join(list(map(str, m_arr))))
+
+p1 = p2 = 0
+c = []
+
+while p1 < n and p2 < m:
+    if a[p1] <= b[p2]:
+        c.append(a[p1])
+        p1 += 1
+    else:
+        c.append(b[p2])
+        p2 += 1
+
+if p1 < n:
+    c = c + a[p1:]
+if p2 < m :
+    c = c + b[p2:]
+print(c)
