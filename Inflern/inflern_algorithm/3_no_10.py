5969d8ab872cc2c19635dc38b0a816decdb1adea
Jisu Choi <gjkl55@naver.com>
2021-09-09 22:33:06 +0900

    스토쿠 검사

---
+++ Inflern/inflern_algorithm/3_no_10.py
@@ -0,0 +1,35 @@
+#스토쿠 검사
+import sys
+# sys.stdin = open("input.txt", "rt")
+
+
+def check(arr):
+    for i in range(9):
+        ch1 = [0] * 10
+        ch2 = [0] * 10
+
+        for j in range(9):
+            ch1[arr[i][j]] = 1
+            ch2[arr[j][i]] = 1
+
+        if sum(ch1) != 9 or sum(ch2) != 9:
+            return False
+
+    for i in range(3):
+        for j in range(3):
+            ch3 = [0] * 10
+
+            for k in range(3):
+                for r in range(3):
+                    ch3[arr[i * 3 + k][j * 3 + r]] = 1
+            if sum(ch3) != 9:
+                return False
+
+    return True
+
+
+matrix = [list(map(int, input().split())) for _ in range(9)]
+if check(matrix):
+    print('YES')
+else:
+    print('NO')
