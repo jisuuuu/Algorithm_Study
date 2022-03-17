abd9b7ad6cdf75d7055dfc518c3d0d5530fa93bd
Jisu Choi <gjkl55@naver.com>
2022-03-17 20:19:19 +0900

    BOJ15688 수 정렬하기 5

--- Baekjoon/Baekjoon_python/boj_11931.py
+++ Baekjoon/Baekjoon_python/boj_11931.py
@@ -4,4 +4,4 @@ n = int(sys.stdin.readline().rstrip())
 nums = []
 for _ in range(n):
     nums.append(int(sys.stdin.readline().rstrip()))
-print('\n'.join(list(map(str, sorted(nums, reverse=True)))))
+print('\n'.join(list(map(str, sorted(nums))))) #수 정렬하기 5 reverse=True 빼기
