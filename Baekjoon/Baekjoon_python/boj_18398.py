c4240369df2785f218682b3f946a599c7788305a
Jisu Choi <gjkl55@naver.com>
2022-03-28 20:19:19 +0900

    BOJ18398 HOMWRK

---
+++ Baekjoon/Baekjoon_python/boj_18398.py
@@ -0,0 +1,8 @@
+#HOMWRK
+import sys
+t = int(sys.stdin.readline().rstrip())
+for _ in range(t):
+    n = int(sys.stdin.readline().rstrip())
+    for _ in range(n):
+        a, b = map(int, sys.stdin.readline().rstrip().split())
+        print(a + b, a * b)
