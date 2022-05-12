237e4ce040e3962863b30736e1ed65e54fed98f4
gjkl55 <gjkl55@naver.com>
2022-05-12 20:19:19 +0900

    BOJ6810 ISBN

---
+++ Baekjoon/Baekjoon_python/boj_6810.py
@@ -0,0 +1,7 @@
+#ISBN
+import sys
+a = int(sys.stdin.readline().rstrip())
+b = int(sys.stdin.readline().rstrip())
+c = int(sys.stdin.readline().rstrip())
+
+print(f'The 1-3-sum is {91 + a + b * 3 + c}')
