160408906583bf126099679ada0745da2bcd9414
gjkl55 <gjkl55@naver.com>
2022-08-31 20:19:19 +0900

    BOJ2864 5와 6의 차이

---
+++ Baekjoon/Baekjoon_python/boj_2864.py
@@ -0,0 +1,7 @@
+#5와 6의 차이
+import sys
+a, b = sys.stdin.readline().rstrip().split()
+
+min_num = int(a.replace('6', '5')) + int(b.replace('6', '5'))
+max_num = int(a.replace('5', '6')) + int(b.replace('5', '6'))
+print(min_num, max_num)
