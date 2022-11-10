1528bb790717823ebe93f2ec0eb8d89cf1fec5f7
gjkl55 <gjkl55@naver.com>
2022-11-10 20:19:19 +0900

    BOJ11279 최대 힙

---
+++ Baekjoon/Baekjoon_python/boj_11279.py
@@ -0,0 +1,15 @@
+#최대 힙
+import sys, heapq
+n = int(sys.stdin.readline().rstrip())
+heap = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline().rstrip())
+
+    if x != 0:
+        heapq.heappush(heap, -x)
+    else:
+        try:
+            print(-1 * heapq.heappop(heap))
+        except:
+            print(0)
