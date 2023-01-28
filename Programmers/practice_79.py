0048db860229ec8628a6caa8d490ecb78f39e72b
jisuuuu <gjkl55@naver.com>
2023-01-28 20:38:38 +0900

    모음 제거

---
+++ Programmers/practice_79.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    arr = ['a', 'e', 'i', 'o', 'u']
+
+    for a in arr:
+        my_string = my_string.replace(a, '')
+    return my_string
+
+
+if __name__ == '__main__':
+    print(solution('bus'))
+    print(solution('nice to meet you'))
