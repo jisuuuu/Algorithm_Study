7d5add3de24f53ec260d2f7e58d1cd1f5b2af525
jisuuuu <gjkl55@naver.com>
2023-03-30 20:19:19 +0900

    숫자 비교하기

---
+++ Programmers/practice_86.py
@@ -0,0 +1,9 @@
+# 숫자 비교하기
+def solution(num1, num2):
+    return 1 if num1 == num2 else -1
+
+
+if __name__ == '__main__':
+    print(solution(2, 3))
+    print(solution(11, 11))
+    print(solution(7, 38))
